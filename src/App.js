1a369ef4f9136c44ea3973fef08a873e3b0daad6
Danila <syvs0990@mail.ru>
2022-02-25 23:38:03 +0700

    Added font, created HTML frame and uploaded images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,224 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.scss"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <div className="main">
+                <div className="container">
+                    <div className="main__head">
+                        <div className="main__logo">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="main__nav">
+                            <div className="main__nav-item">About</div>
+                            <div className="main__nav-item">News</div>
+                            <div className="main__nav-item">Music</div>
+                            <div className="main__nav-item">Media</div>
+                            <div className="main__nav-item">Tours</div>
+                            <div className="main__nav-item">Contacts</div>
+                        </div>
+                        <div className="main__content">
+                            <div className="main__title">New Single</div>
+                            <div className="main__song-name">War For Love</div>
+                            <div className="main__song"></div>
+                            <div className="main__list">
+                                <div className="main__item">
+                                    <img src="" alt="" />
+                                    <div className="main__item-text">Working on my upcoming full-lenth album that`s releasing later this year.</div>
+                                </div>
+                                <div className="main__item">
+                                    <img src="" alt="" />
+                                    <div className="main__item-text">Halloween vibes. Listen my new track!</div>
+                                </div>
+                                <div className="main__item">
+                                    <img src="" alt="" />
+                                    <div className="main__item-text">WarForLove is OUT NOW!! Stream it here!</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="musician">
+                <div className="container">
+                    <div className="musician__content">
+                        <div className="musician__first-block">
+                            <div className="musician__title">Bright Lights</div>
+                            <div className="musician__text">Bright Lights is a multi-Grammy nominated singer, songwriter, DJ and record producer. She has written for numerous  pop stars including Britney Spears, Justin Bieber, Usher and Beyoncé. Her catalog has amassed over 1 billion streams worldwide. More than 100 million of those streams can be attributed to her artist career and include such hits as Porter Robinson's "Language," 3LAU's "How You Love Me" and her own single "Runaway." She was also a featured vocalist on Zedd's #1 Clarity album. Her latest music video, "Put It Down," reached 1 million streams in the first week, releasing independently. Bright Lights is currently in the studio working on a self-produced album slated for release in 2020. </div>
+                            <div className="musician__about">
+                                <div className="musician__item">Based in: Los Angeles</div>
+                                <div className="musician__item">Founded in 2011</div>
+                                <div className="musician__item">Genre: #DancePop </div>
+                                <div className="musician__item">Label: 333 Recordings</div>
+                            </div>
+                        </div>
+                        <div className="musician__second-block">
+                            <div className="musician__image">
+                                <img src="" alt="" />
+                                <img src="" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="quote">
+                <div className="container">
+                    <div className="quote__content">
+                        <div className="quote__first-block">
+                            <div className="quote__text">“In an age where mainstream music is designed to be as easily consumed as possible, listening to Bright Lights is refreshing, to say the least. Her sound is perfect for radio but complex enough to separate her from other pop newcomers.”</div>
+                            <div className="quote__link"><a href="/">PopULove.net</a></div>
+                        </div>
+                        <div className="quote__second-block">
+                            <div className="quote__image">
+                                <img src="" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="tracks">
+                <div className="container">
+                    <div className="tracks__content">
+                        <div className="tracks__first-block">
+                            <div className="tracks__image">
+                                <img src="" alt="" />
+                            </div>
+                        </div>
+                        <div className="tracks__second-block">
+                            <div className="tracks__title"></div>
+                            <div className="tracks__song"></div>
+                            <div className="tracks__list">
+                                <div className="tracks__item">3LAU, Bright Lights — How You Love Me</div>
+                                <div className="tracks__item">Bright Lights, Kaleena Zanders, Kandy — War For Love</div>
+                                <div className="tracks__item">Pink Is Punk, Benny Benassi, Bright Lights — Ghost</div>
+                                <div className="tracks__item">Hardwell, Dyro, Bright Lights — Never Say Goodbye</div>
+                                <div className="tracks__item">Zeds Dead, Dirtyphonics, Bright Lights — Where Are You Now</div>
+                                <div className="tracks__item">Zedd, Bright Lights — Follow You Down</div>
+                            </div>
+                            <div className="tracks__social-media">
+                                <div className="tracks__follow">Follow me:</div>
+                                <div className="tracks__spotify">Spotify</div>
+                                <div className="tracks__itunes">iTunes</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="photos">
+                <div className="container">
+                    <div className="photos__content">
+                        <div className="photos__column">
+                            <div className="photos__image">
+                                <img src="" alt="" />
+                            </div>
+                        </div>
+                        <div className="photos__column">
+                            <div className="photos__image">
+                                <img src="" alt="" />
+                            </div>
+                            <div className="photos__image">
+                                <img src="" alt="" />
+                            </div>
+                            <div className="photos__image">
+                                <img src="" alt="" />
+                                <div className="photos__text">EDC Las Vegas Mainstage</div>
+                            </div>
+                        </div>
+                        <div className="photos__column">
+                            <div className="photos__image">
+                                <img src="" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="slider">
+                <div className="slider__content">
+                    <div className="slider__arrow"></div>
+                    <div className="slider__item">
+                        <div className="slider__image">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="slider__location">Chandler, AZ</div>
+                        <div className="slider__place">The Park at Wild Horse Pass</div>
+                        <div className="slider__item-bottom">
+                            <div className="slider__date">Apr 02 2021</div>
+                            <div className="slider__button">tickets</div>
+                        </div>
+                    </div>
+                    <div className="slider__item">
+                        <div className="slider__image">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="slider__location">Ibiza, IBZ</div>
+                        <div className="slider__place">Swag Ibiza Club</div>
+                        <div className="slider__item-bottom">
+                            <div className="slider__date">Jul 08 2021</div>
+                            <div className="slider__button">tickets</div>
+                        </div>
+                    </div>
+                    <div className="slider__item">
+                        <div className="slider__image">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="slider__location">Ibiza, IBZ</div>
+                        <div className="slider__place">El Swing Ibiza</div>
+                        <div className="slider__item-bottom">
+                            <div className="slider__date">Jul 10 2021</div>
+                            <div className="slider__button">tickets</div>
+                        </div>
+                    </div>
+                    <div className="slider__arrow"></div>
+                </div>
+            </div>
+            <div className="foot">
+                <div className="container">
+                    <div className="foot__content">
+                        <div className="foot__logo">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="foot__management">
+                            <div className="foot__title">Artist Management:<br/>EMAIL:</div>
+                            <div className="foot__text">Jake Henny<br/>jake@brightlightsofficial.com</div>
+                        </div>
+                        <div className="foot__links">
+                            <div className="foot__link">
+                                <a href="/">
+                                    <img src="" alt="" />
+                                </a>
+                            </div>
+                            <div className="foot__link">
+                                <a href="/">
+                                    <img src="" alt="" />
+                                </a>
+                            </div>
+                            <div className="foot__link">
+                                <a href="/">
+                                    <img src="" alt="" />
+                                </a>
+                            </div>
+                            <div className="foot__link">
+                                <a href="/">
+                                    <img src="" alt="" />
+                                </a>
+                            </div>
+                            <div className="foot__link">
+                                <a href="/">
+                                    <img src="" alt="" />
+                                </a>
+                            </div>
+                            <div className="foot__link">
+                                <a href="/">
+                                    <img src="" alt="" />
+                                </a>
+                            </div>
+                        </div>
+                        <div className="foot__copyright">©  2020 DJ bright lights.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
